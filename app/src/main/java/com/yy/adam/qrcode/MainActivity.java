fca5297f0cf46f25a04a2a334dae59ffabb5a55f
jianelegant <jian.elegant@gmail.com>
2018-08-21 19:52:51 +0800

    use lib

--- app/src/main/java/com/yy/adam/qrcode/MainActivity.java
+++ app/src/main/java/com/yy/adam/qrcode/MainActivity.java
@@ -1,13 +1,81 @@
 package com.yy.adam.qrcode;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+
+import com.journeyapps.barcodescanner.CaptureManager;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private CaptureManager mCaptureManager;
+    private DecoratedBarcodeView mDecoratedBarcodeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews(savedInstanceState);
+    }
+
+    private void initViews(Bundle savedInstanceState) {
+        mDecoratedBarcodeView = findViewById(R.id.zxing_barcode_scanner);
+        mDecoratedBarcodeView.setTorchListener(new DecoratedBarcodeView.TorchListener() {
+            @Override
+            public void onTorchOn() {
+
+            }
+
+            @Override
+            public void onTorchOff() {
+
+            }
+        });
+
+        mCaptureManager = new CaptureManager(this, mDecoratedBarcodeView);
+        mCaptureManager.initializeFromIntent(getIntent(), savedInstanceState);
+        mCaptureManager.decode();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCaptureManager.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mCaptureManager.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mCaptureManager.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mCaptureManager.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return mDecoratedBarcodeView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.e(TAG, data.toString());
     }
 }
