1ee9ad7586ddc2c543de61b340639a8dd8eaae0a
jianelegant <jian.elegant@gmail.com>
2018-08-27 22:41:33 +0800

    flur, ad

--- app/src/main/java/com/yy/adam/qrcode/MainApp.java
+++ app/src/main/java/com/yy/adam/qrcode/MainApp.java
@@ -3,6 +3,9 @@ package com.yy.adam.qrcode;
 import android.app.Application;
 import android.content.Context;
 
+import com.flurry.android.FlurryAgent;
+import com.google.android.gms.ads.MobileAds;
+
 public class MainApp extends Application{
 
     public static Context s_GlobalCtx;
@@ -11,5 +14,21 @@ public class MainApp extends Application{
     public void onCreate() {
         super.onCreate();
         s_GlobalCtx = this;
+
+        someInit();
+    }
+
+    private void someInit() {
+        initFlur();
+        initAd();
+    }
+
+    private void initFlur() {
+        new FlurryAgent.Builder()
+                .build(this, "7PGYWWCTZS9VFDJR8YNB");
+    }
+
+    private void initAd() {
+        MobileAds.initialize(this, "ca-app-pub-5644941632262899~9662224413");
     }
 }
