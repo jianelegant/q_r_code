19a5a85ef2cb7c59d0d1e3cbcb804897af1d4c4e
jianelegant <jian.elegant@gmail.com>
2018-08-27 18:51:05 +0800

    various result

--- app/src/main/java/com/yy/adam/qrcode/ResultActivity.java
+++ app/src/main/java/com/yy/adam/qrcode/ResultActivity.java
@@ -3,10 +3,14 @@ package com.yy.adam.qrcode;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.zxing.client.result.EmailAddressParsedResult;
 import com.google.zxing.client.result.ParsedResult;
+import com.google.zxing.client.result.SMSParsedResult;
+import com.google.zxing.client.result.TelParsedResult;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -47,13 +51,25 @@ public class ResultActivity extends AppCompatActivity {
                     ResultHandler.openBrowser(this, parsedResult.getDisplayResult());
                     break;
                 case TEL:
+                    TelParsedResult telResult = (TelParsedResult) parsedResult;
+                    if(!TextUtils.isEmpty(telResult.getTelURI())) {
+                        ResultHandler.dialPhoneFromUri(this, telResult.getTelURI());
+                    } else {
+                        ResultHandler.dialPhone(this, telResult.getNumber());
+                    }
                     break;
+                case EMAIL_ADDRESS:
+                    EmailAddressParsedResult emailResult = (EmailAddressParsedResult) parsedResult;
+                    ResultHandler.sendEmail(this, emailResult.getTos(), emailResult.getCCs(), emailResult.getBCCs(), emailResult.getSubject(), emailResult.getBody());
+                    break;
+                case SMS:
+                    SMSParsedResult smsResult = (SMSParsedResult) parsedResult;
+                    ResultHandler.sendSMS(this, smsResult.getNumbers()[0], smsResult.getBody());
+                    break;
+                case TEXT:
                 case ADDRESSBOOK:
                 case GEO:
-                case EMAIL_ADDRESS:
                 case PRODUCT:
-                case TEXT:
-                case SMS:
                 case CALENDAR:
                 case WIFI:
                 case ISBN:
--- app/src/main/java/com/yy/adam/qrcode/ResultHandler.java
+++ app/src/main/java/com/yy/adam/qrcode/ResultHandler.java
@@ -9,6 +9,14 @@ import android.support.v7.app.AlertDialog;
 
 public class ResultHandler {
 
+    public static void dialPhone(Activity activity, String phoneNumber) {
+        launchIntent(activity, new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber)));
+    }
+
+    public static void dialPhoneFromUri(Activity activity, String uri) {
+        launchIntent(activity, new Intent(Intent.ACTION_DIAL, Uri.parse(uri)));
+    }
+
     public static void openBrowser(Activity activity, String url) {
         if (url.startsWith("HTTP://")) {
             url = "http" + url.substring(4);
@@ -17,6 +25,45 @@ public class ResultHandler {
         }
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
         launchIntent(activity, intent);
+    }
+
+    public static void sendEmail(Activity activity, String[] to,
+                         String[] cc,
+                         String[] bcc,
+                         String subject,
+                         String body) {
+        Intent intent = new Intent(Intent.ACTION_SEND, Uri.parse("mailto:"));
+        if (to != null && to.length != 0) {
+            intent.putExtra(Intent.EXTRA_EMAIL, to);
+        }
+        if (cc != null && cc.length != 0) {
+            intent.putExtra(Intent.EXTRA_CC, cc);
+        }
+        if (bcc != null && bcc.length != 0) {
+            intent.putExtra(Intent.EXTRA_BCC, bcc);
+        }
+        putExtra(intent, Intent.EXTRA_SUBJECT, subject);
+        putExtra(intent, Intent.EXTRA_TEXT, body);
+        intent.setType("text/plain");
+        launchIntent(activity, intent);
+    }
+
+    public static void sendSMS(Activity activity, String phoneNumber, String body) {
+        sendSMSFromUri(activity, "smsto:" + phoneNumber, body);
+    }
+
+    private static void sendSMSFromUri(Activity activity, String uri, String body) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse(uri));
+        putExtra(intent, "sms_body", body);
+        // Exit the app once the SMS is sent
+        intent.putExtra("compose_mode", true);
+        launchIntent(activity, intent);
+    }
+
+    private static void putExtra(Intent intent, String key, String value) {
+        if (value != null && !value.isEmpty()) {
+            intent.putExtra(key, value);
+        }
     }
 
     private static void launchIntent(final Activity activity, Intent intent) {
